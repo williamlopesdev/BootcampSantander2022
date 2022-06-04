ab448903c41bf87fb94902ed9c0a4f67023f24b7
williamlopesdev <wilcypherr@gmail.com>
2022-06-04 14:48:00 -0300

    compara numeros

---
+++ INTRODUCAO_js/ATV_01/comparaNumeros.js
@@ -0,0 +1,49 @@
+
+/*Sintaxe e Operadores
+Este repositório contém a atividade prática do Curso "Sintaxe e Operadores", que faz parte do Basecamp de Javascript que minstrei pela Digital Innovation One.
+
+Atividade
+Crie uma função que recebe dois números como parâmetros.
+Confira se os números são iguais.
+Confira se a soma dos números é maior que 10 ou menor que 20.
+Retorne uma string dizendo "Os números num1 e num2 não/são iguais. Sua soma é soma, que é maior/menor que 10 e maior/menor que 20".
+Exemplo:
+
+Input: 1, 2
+Output: Os números 1 e 2 não são iguais. Sua soma é 3, que é menor que 10 e menor que 20.*/
+
+function comparaNumeros(num1, num2) {
+	const primeiraFrase = criaPrimeiraFrase(num1, num2);
+	const segundaFrase = criaSegundaFrase(num1, num2);
+
+	return `${primeiraFrase} ${segundaFrase}`;
+}
+
+function criaPrimeiraFrase(num1, num2) {
+	let primeiraFrase = `Os números ${num1} e ${num2}`;
+	let simNao = 'não';
+
+	if (num1 === num2) {
+		simNao = '';
+	}
+
+	return `${primeiraFrase} ${simNao} são iguais.`;
+}
+
+function criaSegundaFrase(num1, num2) {
+	const soma = num1 + num2;
+	let comparaDez = 'menor';
+	let comparaVinte = 'menor';
+
+	if (soma > 10) {
+		comparaDez = 'maior';
+	}
+
+	if (soma > 20) {
+		comparaVinte = 'maior';
+	}
+
+	return `Sua soma é ${soma}, que é ${comparaDez} do que 10 e ${comparaVinte} do que 20.`;
+}
+
+console.log(comparaNumeros(20, 20));
